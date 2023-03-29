4fb608733e5b473b1afe76fddc76ba06a8435886
Realestateguy888 <73314426+Realestateguy888@users.noreply.github.com>
2023-03-28 21:35:58 -0400

    Update draftsql.sql

--- sql/draftsql.sql
+++ sql/draftsql.sql
@@ -1,3 +1,7 @@
+/*
+With this schema, you can store information about users, their roles, and their contact information in the users table. The time_slots table contains 
+start and end times for each available time slot, and the user_time_slots table assigns users to specific time slots on specific dates. 
+*/
 CREATE TABLE users (
   id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
@@ -20,3 +24,9 @@ CREATE TABLE user_time_slots (
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (time_slot_id) REFERENCES time_slots(id)
 );
+/*
+For updating data, you can use INSERT, UPDATE, and DELETE queries. Here's an example of how you could assign a user to a time slot:
+*/
+UPDATE schedule SET user_id = 123 WHERE date = '2023-04-01' AND time_slot = '09:00:00';
+UPDATE schedule SET user_id = 123 WHERE date = '2023-04-01' AND time_slot = '09:00:00';
+DELETE FROM schedule WHERE user_id = 123 AND date = '2023-04-01' AND time_slot = '09:00:00';
